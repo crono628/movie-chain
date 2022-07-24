a315360d963e484e4180e765e855f5157a42b078
crono628 <84817352+crono628@users.noreply.github.com>
2022-07-24 18:05:11 -0400

    rudimentary setup ensuring api call works

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from 'react';
+import Api from './components/Api';
+import Find from './components/Find';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  const config = {
+    api: import.meta.env.VITE_API_KEY,
+    baseUrl: 'https://api.themoviedb.org/3/',
+    imageUrl: 'https://image.tmdb.org/t/p/w1280',
+  };
+
+  const handleClick = async (page = 1) => {
+    let data = [];
+    try {
+      const response = await fetch(
+        `${config.baseUrl}movie/popular?api_key=${config.api}&page=${page}`
+      );
+      const responseData = await response.json();
+      data = responseData?.results;
+      setMovies(data);
+    } catch (error) {}
+    console.log(data);
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="text-4xl w-screen bg-indigo-400 flex justify-center items-center flex-col">
+      <Find />
+      <Api handleClick={handleClick} />
+      <button onClick={() => setMovies(null)}>clear</button>
+      <div className="grid grid-cols-4">
+        {movies?.map((movie) => (
+          <div key={movie.id} className="w-fit text-sm m-2">
+            <img src={`${config.imageUrl}${movie.poster_path}`} />
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Find.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Find = () => {
+  return <div className="bg-lime-400 flex">Find</div>;
+};
+
+export default Find;
